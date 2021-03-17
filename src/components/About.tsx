268e12f8b118322656d38f123e7857315cae40f7
Carlos Duarte <40669751+CarlosFDuarte8@users.noreply.github.com>
2021-03-17 13:47:23 -0300

    Initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,164 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+
+class About extends React.Component {
+  
+  
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="box-shadow-full">
+                            <div className="row">
+
+                                <div className="col-md-0">
+                                    <div className="box-shadow-full2">
+                                        <div className="col-md-2"
+                                             style={{ margin: "0 auto" }}
+                                             >
+                                             <div
+                                                className="about-img"
+                                                style={{ textAlign: "center" }}
+                                                >
+                                                <img 
+                                                    className="img-fluid rounded b-shadow-a"
+                                                    src="https://www.w3.org/html/logo/badge/html5-badge-h-solo.png" 
+                                                    width="64" height="64" alt="HTML5 Powered" title="HTML5 Powered" />
+                                            </div>
+                                        </div>
+                                        <div className="skill-mf">
+                                            <p class="material-icons"
+                                               style={{ textAlign: "center" }}
+                                               >HTML5</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-0">
+                                    <div className="box-shadow-full2">
+                                        <div className="col-md-2"
+                                             style={{ margin: "0 auto" }}
+                                             >
+                                             <div
+                                                className="about-img"
+                                                style={{ textAlign: "center" }}
+                                                >
+                                                <img 
+                                                    className="img-fluid rounded b-shadow-a"
+                                                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" 
+                                                    width="64" height="64" alt="HTML5 Powered" title="HTML5 Powered" />
+                                            </div>
+                                        </div>
+                                        <div className="skill-mf">
+                                            <p class="material-icons"
+                                               style={{ textAlign: "center" }}
+                                               >React</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-0">
+                                    <div className="box-shadow-full2">
+                                        <div className="col-md-2"
+                                             style={{ margin: "0 auto" }}
+                                             >
+                                             <div
+                                                className="about-img"
+                                                style={{ textAlign: "center" }}
+                                                >
+                                                <img 
+                                                    className="img-fluid rounded b-shadow-a"
+                                                    src="https://angular.io/assets/images/logos/angular/angular.svg" 
+                                                    width="64" height="64" alt="HTML5 Powered" title="HTML5 Powered" />
+                                            </div>
+                                        </div>
+                                        <div className="skill-mf">
+                                            <p class="material-icons"
+                                               style={{ textAlign: "center" }}
+                                               >Angular</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-0">
+                                    <div className="box-shadow-full2">
+                                        <div className="col-md-2"
+                                             style={{ margin: "0 auto" }}
+                                             >
+                                             <div
+                                                className="about-img"
+                                                style={{ textAlign: "center" }}
+                                                >
+                                                <img 
+                                                    className="img-fluid rounded b-shadow-a"
+                                                    src="https://www.php.net/images/logos/new-php-logo.svg" 
+                                                    width="64" height="64" alt="HTML5 Powered" title="HTML5 Powered" />
+                                            </div>
+                                        </div>
+                                        <div className="skill-mf">
+                                            <p class="material-icons"
+                                               style={{ textAlign: "center" }}
+                                               >PHP</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-0">
+                                    <div className="box-shadow-full2">
+                                        <div className="col-md-2"
+                                             style={{ margin: "0 auto" }}
+                                             >
+                                             <div
+                                                className="about-img"
+                                                style={{ textAlign: "center" }}
+                                                >
+                                                <img 
+                                                    className="img-fluid rounded b-shadow-a"
+                                                    src="https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png" 
+                                                    width="64" height="64" alt="HTML5 Powered" title="HTML5 Powered" />
+                                            </div>
+                                        </div>
+                                        <div className="skill-mf">
+                                            <p class="material-icons"
+                                               style={{ textAlign: "center" }}
+                                               >JavaScript</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-0">
+                                    <div className="box-shadow-full2">
+                                        <div className="col-md-2"
+                                             style={{ margin: "0 auto" }}
+                                             >
+                                             <div
+                                                className="about-img"
+                                                style={{ textAlign: "center" }}
+                                                >
+                                                <img 
+                                                    className="img-fluid rounded b-shadow-a"
+                                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaHs5InZUtPX-ZY8xmu157NyzQaVjqsJqHnQ&usqp=CAU" 
+                                                    width="64" height="64" alt="HTML5 Powered" title="HTML5 Powered" />
+                                            </div>
+                                        </div>
+                                        <div className="skill-mf">
+                                            <p class="material-icons"
+                                               style={{ textAlign: "center" }}
+                                               >Java</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+  }
+}
+
+export default About;
